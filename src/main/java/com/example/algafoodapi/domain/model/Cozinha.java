98a17933a9250621d1ae280ad37d6e3cec37bf5e
Miguel Fernandes <miguel.fernandes275@gmail.com>
2023-09-01 11:04:13 -0300

    Iniciando os trabalhos

--- src/main/java/com/example/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/com/example/algafoodapi/domain/model/Cozinha.java
@@ -1,2 +1,48 @@
-package com.example.algafoodapi.domain.model;public class Cozinha {
+package com.example.algafoodapi.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
+
+@Entity
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/example/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/example/algafoodapi/domain/model/Restaurante.java
@@ -1,2 +1,57 @@
-package com.example.algafoodapi.domain.model;public class Restaurante {
+package com.example.algafoodapi.domain.model;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxafrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxafrete() {
+        return taxafrete;
+    }
+
+    public void setTaxafrete(BigDecimal taxafrete) {
+        this.taxafrete = taxafrete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurante that = (Restaurante) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
