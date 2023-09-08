e033beb029a65533964ee761603d73bcb12cfa42
Miguel Fernandes <miguel.fernandes275@gmail.com>
2023-09-08 16:06:35 -0300

    Dando seguimento no Projeto

--- src/main/java/com/example/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/example/algafoodapi/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import com.example.algafoodapi.domain.model.Cozinha;
 import com.example.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
+    }
+
+@GetMapping("/{cozinhaId}" )
+    public Cozinha buscar(@PathVariable Long cozinhaId){
+        return cozinhaRepository.buscar(cozinhaId);
     }
 
 }
